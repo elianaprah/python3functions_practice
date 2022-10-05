a2bce755cb3617a393b48f752554d02d69d126ca
elianaprah <99228580+elianaprah@users.noreply.github.com>
2022-10-05 19:22:06 -0400

    Add files via upload

---
+++ python_projects/functions_practice.py
@@ -0,0 +1,22 @@
+def hello():
+    print('Hello, earthling')
+
+def pack(a,b,c):
+    return [a,b,c]
+
+def eat_lunch(mylst):
+    if len(my_lst) == 0:
+        print("My lunchbox is empty!")
+    else:
+     for i in range(len(my_lst)):
+            if i == 0:
+                print(f"First I eat {my_lst[0]}")
+            else:
+                print(f"Next I eat {my_lst[i]}")
+                6
+                
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch(["po-boy"])
+eat_lunch(["chicken-salad","sashimi","po-boy", "halal"])
